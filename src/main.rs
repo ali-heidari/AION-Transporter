0ab5f0c6f0e661080c1d2194a8f07c9f60c30902
ali-heidari <ali-heidari@outlook.com>
2025-11-24 01:16:26 +0330

    Create serve command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use aion_transporter::quic::server::start_quic;
 use anyhow::{Ok, Result};
+use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)] // Provides standard help/version text
@@ -8,8 +10,28 @@ struct Cli {
     command: Commands,
 }
 
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Runs QUIC server
+    Serve {
+        /// The server port
+        port: u32,
+    },
+}
+
+fn on_message_received(ip: String, message: String) {
+    print!("{}: {}", ip, message);
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let commands = Cli::parse();
+    match &commands.command {
+        Commands::Serve { port } => {
+            println!("Executing SERVE command:");
+            println!("  Port:      {}", port);
+            start_quic(*port, on_message_received).await?;
+        }
+    }
     Ok(())
 }
